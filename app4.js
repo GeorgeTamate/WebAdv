283f88b1e1c5fe5f34edf4625bc4b3d12f638a55
Keiko Tamate <Keiko Tamate>
2017-01-23 18:30:29 -0400

    All path functions set but yet to be implemented.

--- app4.js
+++ app4.js
@@ -5,23 +5,56 @@
 var express = require('express')
 var app = express()
 
-app.get('*', function (req, res) {
-  //res.send('Hello World!');
-  
-  var portnumber =  req.get('host');
-  portnumber = portnumber.split(':')[1];
 
-  var myjson = {
-      "httpMethod": req.method,
-      "path": req.path,
-      "port": portnumber,
-      "headerFields": req.headers
-  }
-
-  res.send(myjson);
-  console.log(myjson);
+app.get('/404', function (req, res) {
+    console.log('not found')
+    res.send('not found');
 })
 
+app.get('/protected', function (req, res) {
+    console.log('protected')
+    res.send('protected');
+})
+
+app.get('/error', function (req, res) {
+    console.log('error')
+    res.send('error');
+})
+
+app.get('/nonimplemented', function (req, res) {
+    console.log('nonimplemented')
+    res.send('nonimplemented');
+})
+
+app.get('/login', function (req, res) {
+    console.log('login')
+    res.send('login');
+})
+
+app.get('*', function (req, res) {
+
+    var portNumber = req.get('host');
+    portNumber = portNumber.split(':')[1];
+
+    var myjson = {
+        "httpMethod": req.method,
+        "path": req.path,
+        "port": portNumber,
+        "headerFields": req.headers
+    }
+
+    res.send(myjson);
+})
+
+app.post('/login', function (req, res) {
+    res.send('No resource found for this path.');
+})
+
+app.post('*', function (req, res) {
+    res.send('No resource found for this path.');
+})
+
+
 app.listen(8085, function () {
-  console.log('Example app listening on port 8085!');
+    console.log('Example app listening on port 8085!');
 })
