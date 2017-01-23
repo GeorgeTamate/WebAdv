6ddac7bed4d2339d0f0ebd66637cf9e35a1b676b
Keiko Tamate <Keiko Tamate>
2017-01-23 19:41:46 -0400

    Added Resources folder with login.html file.

--- app4.js
+++ app4.js
@@ -2,36 +2,42 @@
 // and Header fields as a JSON object on the body of the Web 
 // server's response.
 
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
+var path = require('path');
 
 app.get('/404', function (req, res) {
-    console.log('not found')
+    res.statusCode = 404;
+    console.log('not found');
     res.send('not found');
 })
 
 app.get('/protected', function (req, res) {
-    console.log('protected')
+    res.statusCode = 401;
+    console.log('protected');
     res.send('protected');
 })
 
 app.get('/error', function (req, res) {
-    console.log('error')
+    res.statusCode = 500;
+    console.log('error');
     res.send('error');
 })
 
 app.get('/nonimplemented', function (req, res) {
-    console.log('nonimplemented')
+    res.statusCode = 501;
+    console.log('nonimplemented');
     res.send('nonimplemented');
 })
 
 app.get('/login', function (req, res) {
-    console.log('login')
-    res.send('login');
+    res.statusCode = 200;
+    console.log('login');
+    res.sendFile(path.join(__dirname + '/Resources/login.html'));
 })
 
 app.get('*', function (req, res) {
+    
 
     var portNumber = req.get('host');
     portNumber = portNumber.split(':')[1];
@@ -43,14 +49,17 @@ app.get('*', function (req, res) {
         "headerFields": req.headers
     }
 
+    res.statusCode = 200;
     res.send(myjson);
 })
 
 app.post('/login', function (req, res) {
-    res.send('No resource found for this path.');
+    res.statusCode = 200;
+    res.send('Password Cual es.');
 })
 
 app.post('*', function (req, res) {
+    res.statusCode = 200;
     res.send('No resource found for this path.');
 })
 
